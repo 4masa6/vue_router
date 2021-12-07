ae4225cbc4cb787ec7c6d8e37e8dc5c68966cda2
mogata <ms46.dev@gmail.com>
2021-12-07 22:07:03 +0900

    JSXコンポーネントの読み込み

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div id="app">
+    <HelloJSX />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloJSX from './components/HElloJSX'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloJSX
   }
 }
 </script>
