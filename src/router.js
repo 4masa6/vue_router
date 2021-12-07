e3459afe82c93bab13d5c0d90f2d5dd0ac65c940
mogata <ms46.dev@gmail.com>
2021-12-07 22:28:58 +0900

    router.js作成

---
+++ src/router.js
@@ -0,0 +1,25 @@
+// まずはimport文でVueRouterから必要な関数などを用意する。
+// ルーティングで割り当てるコンポーネント類も用意しておく。
+import { createRouter, createWebHistory} from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import HelloJSX from './components/HelloJSX.jsx'
+
+// export 値 で、そのスクリプトファイルをimportした時に指定の値が取り出せるようになる。
+// routerオブジェクトは、スクリプトを作成後、Vue3のアプリケーションに組み込む必要がある。
+// このため、exportを使ってrouterオブジェクトが外部から利用できるようにする。
+export const router = createRouter({
+  // createRouterの引数のオブジェクトにhistoryとroutesという値を用意する
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',             // 割り当てるパス
+      name: 'index',         // 名前
+      component: HelloWorld, // 割り当てるコンポーネント
+    },
+    {
+      path: '/jsx',
+      name: 'jsx',
+      component: HelloJSX,
+    }
+  ]
+})
